e3527696e2fe388d73047e139db515655935a91f
Pierrick <barbarroux.pierrick@outlook.fr>
2019-01-13 18:51:46 +0100

    Tentative bonne avec la reflexion

--- Constantes.h
+++ Constantes.h
@@ -16,7 +16,7 @@ const int SCREEN_HEIGHT = 720; //Résolution verticale
 const double ANGLE_STEP_HORIZONTAL = VIEW_ANGLE_HORIZONTAL / SCREEN_WIDTH;
 const double ANGLE_STEP_VERTICAL = VIEW_ANGLE_VERTICAL / SCREEN_WIDTH;
 const int N_SCENE_OBJECTS = 7+1; //Doit correspondre au nombre exact de "Shape" dans la scène.
-const double SCENE_DEPTH = 1; //Profondeur de la scène
-const double SCENE_HEIGHT = 1; //Hauteur de la scène
-const double SCENE_WIDTH = 1; //Largeur de la scène
+const double SCENE_DEPTH = 2; //Profondeur de la scène
+const double SCENE_HEIGHT = 2; //Hauteur de la scène
+const double SCENE_WIDTH = 2; //Largeur de la scène
 #endif // !HEADER_CONSTANTES_H
