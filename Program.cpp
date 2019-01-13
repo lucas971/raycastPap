9fd553966ec0e9add9abf3ee619f0c4268df84fc
Pierrick <barbarroux.pierrick@outlook.fr>
2019-01-13 18:33:16 +0100

    autres tentatives

--- Program.cpp
+++ Program.cpp
@@ -58,14 +58,14 @@ int main(){
 	shapeList[7] = qu6;
 
 	//Step 3 : Creation des objets 3D
-	Shape* sph1 = new Sphere(Material(200, 0, 0, 0), 
-		Vector3f(mainCamera.position_.x_ + .2, mainCamera.position_.y_, mainCamera.position_.z_ + 0.4),
+	Shape* sph1 = new Sphere(Material(200, 0, 0, 0.3), 
+		Vector3f(mainCamera.position_.x_ + 0.3, mainCamera.position_.y_, mainCamera.position_.z_ + 0.4),
 		(float).2);
 	shapeList[5] = sph1;
 
 	Shape* box1 = new Box(Material(200, 0, 0, 0),
-		Vector3f(mainCamera.position_.x_ -.3, mainCamera.position_.y_- .1, mainCamera.position_.z_ + 0.3),
-		Vector3f(mainCamera.position_.x_ -.1, mainCamera.position_.y_ + .1, mainCamera.position_.z_ + 0.8));
+		Vector3f(mainCamera.position_.x_ -1, mainCamera.position_.y_- .2, mainCamera.position_.z_ + 0.3),
+		Vector3f(mainCamera.position_.x_ -.4, mainCamera.position_.y_ + .7, mainCamera.position_.z_ + 0.8));
 	shapeList[6] = box1;
 
 
